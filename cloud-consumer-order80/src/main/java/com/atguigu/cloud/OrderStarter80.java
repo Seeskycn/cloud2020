b12a365e847e0c3541429fa2112cbcf6beb945c9
Seeskycn <251401995@qq.com>
2020-03-08 10:55:19 +0800

    order入住eureka

--- cloud-consumer-order80/src/main/java/com/atguigu/cloud/OrderStarter80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/cloud/OrderStarter80.java
@@ -2,11 +2,13 @@ package com.atguigu.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderStarter80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderStarter80.class,args);
