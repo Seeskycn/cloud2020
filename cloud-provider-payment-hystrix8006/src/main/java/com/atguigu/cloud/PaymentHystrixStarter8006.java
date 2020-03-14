941b1d5c5bd0001fc5fb1108be54c5a60ff7b596
Seeskycn <251401995@qq.com>
2020-03-14 16:50:04 +0800

    payment 8006 加入 hystrix 仪表盘监控

--- cloud-provider-payment-hystrix8006/src/main/java/com/atguigu/cloud/PaymentHystrixStarter8006.java
+++ cloud-provider-payment-hystrix8006/src/main/java/com/atguigu/cloud/PaymentHystrixStarter8006.java
@@ -1,9 +1,14 @@
 package com.atguigu.cloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+
+import java.security.PublicKey;
 
 /*
  *-----------------神兽保佑 -----------------
@@ -38,6 +43,17 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 @EnableCircuitBreaker
 public class PaymentHystrixStarter8006 {
     public static void main(String[] args) {
+
         SpringApplication.run(PaymentHystrixStarter8006.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
