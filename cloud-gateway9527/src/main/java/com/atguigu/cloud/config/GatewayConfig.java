4a1664e222372a382b8af4e4d28c9f1ec1485ab9
Seeskycn <251401995@qq.com>
2020-03-14 17:41:12 +0800

    加入网关gateway

---
+++ cloud-gateway9527/src/main/java/com/atguigu/cloud/config/GatewayConfig.java
@@ -0,0 +1,50 @@
+package com.atguigu.cloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.support.ipresolver.RemoteAddressResolver;
+import org.springframework.cloud.gateway.support.ipresolver.XForwardedRemoteAddressResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/*
+ *-----------------神兽保佑 -----------------
+ *           ┌─┐        ┌─┐
+ *       ┌─┘  ┴────┘  ┴─┐
+ *       │                        │
+ *       │        ───          │
+ *       │  ─┬┘       └┬─   │
+ *       │                        │
+ *       │       ─┴─           │
+ *       │                        │
+ *       └───┐          ┌──┘
+ *               │          │
+ *               │          │
+ *               │          │
+ *               │          └────────────┐
+ *               │                                    │
+ *               │                                    ├─┐
+ *               │                                    ┌─┘
+ *               │                                    │
+ *               └─┐  ┐  ┌─────┬─┐  ┌──┘
+ *                   │  ┤  ┤          │  ┤  ┤
+ *                   └─┴─┘          └─┴─┘
+ *
+ * ------------------代码无BUG!--------------
+ * @Author: Seesky
+ * @Date: 2020/3/14 17:22
+ * @Description:
+ */
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator getCustomerRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("route_baidu",r ->r.path("/*").uri("http://news.baidu.com/")).build();
+        return routes.build();
+
+    }
+
+}
